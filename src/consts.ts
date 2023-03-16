ea7ce70f69e1a130b9266d04ddc159a94aadb15b
Angel Nieves <nievesaj@gmail.com>
2023-03-16 13:20:59 -0400

    updated site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Astrog Blog Has Been Created 🚀';
+export const SITE_DESCRIPTION = 'Welcome to my Awsome Astro Blog';
